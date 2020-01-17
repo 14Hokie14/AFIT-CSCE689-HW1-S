fd76a0b1360abbdaebdce903132b1e6236cba49b
joshua.white <joshua.white@afit.edu>
2020-01-16 22:48:46 -0500

    o Right now all I have implemented is a simple echo server.

--- src/TCPClient.cpp
+++ src/TCPClient.cpp
@@ -1,12 +1,25 @@
 #include "TCPClient.h"
 
+/**
+ * I am using the following sites as references to create this file: 
+ * https://www.geeksforgeeks.org/socket-programming-cc/
+ * https://www.geeksforgeeks.org/socket-programming-in-cc-handling-multiple-clients-on-server-without-multi-threading/
+ * https://www.geeksforgeeks.org/tcp-and-udp-server-using-select/
+ * https://stackoverflow.com/questions/15673846/how-to-give-to-a-client-specific-ip-address-in-c
+ * http://beej.us/guide/bgnet/html/  // This document is amazing. 
+ * 
+ * NOTE: I am also not going to break out the client code into helper methods as much as I did the server code 
+ * because the code is much more simple and I don't think it would increase readability.  
+ *
+ * Author: Joshua H. White
+**/
 
 /**********************************************************************************************
  * TCPClient (constructor) - Creates a Stdin file descriptor to simplify handling of user input. 
  *
  **********************************************************************************************/
 
-TCPClient::TCPClient() {
+TCPClient::TCPClient() : Client(){
 }
 
 /**********************************************************************************************
@@ -26,6 +39,27 @@ TCPClient::~TCPClient() {
  **********************************************************************************************/
 
 void TCPClient::connectTo(const char *ip_addr, unsigned short port) {
+    // Create the socket for the client
+    this->client_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if( this->client_sock == 0 ){
+        perror("Failure in socket() in TCPClient.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    this->address.sin_family = AF_INET; 
+    this->address.sin_port = htons( port ); // htons is shorthand for Host-to-Network short
+
+    // Convert the IP address to binary form
+    if(inet_pton(AF_INET, ip_addr, &address.sin_addr) <= 0){
+        perror("Invalid address in TCPClient.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Now try to connect
+    if(connect(this->client_sock, (struct sockaddr *)&this->address, sizeof(this->address)) < 0){
+        perror("Failure in connect() in TCPClient.\n");
+        exit(EXIT_FAILURE);
+    }
 
 }
 
@@ -38,7 +72,33 @@ void TCPClient::connectTo(const char *ip_addr, unsigned short port) {
  **********************************************************************************************/
 
 void TCPClient::handleConnection() {
-   
+    // Read in a message  and just echo it for now
+    this->valread = read (client_sock, buf, 1024);
+    buf[this->valread] = '\0';
+    std::cout << buf << std::endl;
+
+    this->valread = 0;
+    char hello[100] = "This is the client";
+    send(this->client_sock, hello, strlen(hello), 0);
+    std::cout << "message sent" << std::endl;
+    this->valread = read (this->client_sock, buf, 1024);
+    buf[this->valread] = '\0';
+    std::cout << buf << std::endl; 
+
+    while(1){
+        std::cout << ">";
+        std::string data;
+        getline(std::cin, data);
+        if(data == "quit"){
+            send(this->client_sock, data.c_str(), strlen(data.c_str()), 0);
+            break;
+        }
+        send(this->client_sock, data.c_str(), strlen(data.c_str()), 0); 
+        std::cout << "message sent" << std::endl;
+        this->valread = read (this->client_sock, buf, 1024);
+        buf[this->valread] = '\0';
+        std::cout << buf << std::endl;
+    }
 }
 
 /**********************************************************************************************
@@ -48,6 +108,7 @@ void TCPClient::handleConnection() {
  **********************************************************************************************/
 
 void TCPClient::closeConn() {
+    close(this->client_sock);
 }
 
 
--- src/TCPServer.cpp
+++ src/TCPServer.cpp
@@ -1,36 +1,181 @@
 #include "TCPServer.h"
 
-TCPServer::TCPServer() {
+/**
+ * I am using the following sites as references to create this file and the rest of my code base: 
+ * https://www.geeksforgeeks.org/socket-programming-cc/
+ * https://www.geeksforgeeks.org/socket-programming-in-cc-handling-multiple-clients-on-server-without-multi-threading/
+ * https://www.geeksforgeeks.org/tcp-and-udp-server-using-select/
+ * https://stackoverflow.com/questions/15673846/how-to-give-to-a-client-specific-ip-address-in-c
+ * http://beej.us/guide/bgnet/html/  // This document is amazing. 
+ *
+ * Author: Joshua H. White
+**/
 
+/**
+* Constructor, does nothing for now. Also calls the constructor for Server. 
+**/
+TCPServer::TCPServer() : Server() {}
+
+/**
+* Destructor, does nothing for now. 
+**/
+TCPServer::~TCPServer() {}
+
+
+/**
+* Helper methods for the server:
+**/
+
+/**
+* Helper method to set up the socket for the server. 
+**/ 
+void TCPServer::sockSetup(){
+    this->server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if( this->server_sock == 0 ){
+        perror("Failure in socket() in TCPServer.\n");
+        exit(EXIT_FAILURE);
+    }
+    this->biggestFD = this->server_sock;
 }
 
 
-TCPServer::~TCPServer() {
-
+/**
+* Helper method that wraps the setsockopt call. 
+*
+* Throws: socket_error for recoverable errors, runtime_error for unrecoverable types
+**/
+void TCPServer::sockOptions(){
+    if ( setsockopt(this->server_sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &this->opt, sizeof(this->opt)) ){
+        perror("Failure in setsockopt() in TCPServer.\n");
+        exit(EXIT_FAILURE);
+    }
 }
+
+
+/**
+* Helper method that wraps the bind call, and any required setup. 
+*
+* Throws: socket_error for recoverable errors, runtime_error for unrecoverable types
+**/
+void TCPServer::sockBind(const char *ip_addr, short unsigned int port){
+    // Set up the sockaddr_in struct here:
+    this->address.sin_family = AF_INET; 
+    // this->address.sin_addr.s_addr = INADDR_ANY; // Won't use this, this uses the computer's default IP of 127.0.0.1
+    inet_pton(AF_INET, ip_addr, &(this->address.sin_addr)); // Inserts the IP address as a string into the sockaddr_in struct
+    this->address.sin_port = htons( port ); // htons is shorthand for Host-to-Network short
+    
+    //std::cout << "here" << std::endl; 
+    
+    // Now bind the socket, if it fails print an error message and exit
+    if ( ( bind(this->server_sock, (struct sockaddr *)&this->address, sizeof(this->address)) ) < 0 ){
+        perror("Failure in bind() in TCPServer.\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+/**
+* Helper method to send messages on a connected server socket
+**/
+   void TCPServer::sockSend(int sock, const char* msg){
+       send(sock, msg, strlen(msg), 0);
+}
+
+
+/**
+* Helper method that just reads in a message and echos the message
+**/
+void TCPServer::readEcho(){
+    zeroBuf();
+    valread = read (new_sock, buf, 1024);
+    std::cout << buf << std::endl;
+    buf[valread] = '\0';
+    sockSend(new_sock, &buf[0]);
+}
+
+
+/**
+* Helper method that checks to see if the passed in buffer says quit
+*
+* Returns: 0 if the parameter is "quit", -1 otherwise
+**/
+int TCPServer::quitCheck(const char *buffer){
+    std::string q = "quit";
+    if(q.compare(buffer) == 0){
+        return 0; 
+    }
+    return -1; 
+}
+
+
+/**
+* Helper method to 0 out buf
+**/
+void TCPServer::zeroBuf(){
+    for(int i = 0; i < 1024; i++){
+        this->buf[i] = '\0';
+    }
+}
+
+//============================================================================================================================================================================
+/** 
+* Given methods to implement below: 
+**/
+
 
 /**********************************************************************************************
  * bindSvr - Creates a network socket and sets it nonblocking so we can loop through looking for
  *           data. Then binds it to the ip address and port
  *
  *    Throws: socket_error for recoverable errors, runtime_error for unrecoverable types
- **********************************************************************************************/
-
+**********************************************************************************************/
 void TCPServer::bindSvr(const char *ip_addr, short unsigned int port) {
+    // Socket setup:
+    sockSetup();
 
-   
+    // Set up the options of the socket: 
+   sockOptions();
+
+    // Bind the socket:
+    sockBind(ip_addr, port);
 }
+
 
 /**********************************************************************************************
  * listenSvr - Performs a loop to look for connections and create TCPConn objects to handle
  *             them. Also loops through the list of connections and handles data received and
- *             sending of data. 
+ *             sending of data.   
  *
  *    Throws: socket_error for recoverable errors, runtime_error for unrecoverable types
- **********************************************************************************************/
-
+**********************************************************************************************/
 void TCPServer::listenSvr() {
+    
 
+    // Intermediate step, we are just going to make an echo server. 
+    if( listen(this->server_sock, 10) < 0){
+        perror("Failure in listen() in TCPServer.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Call accept, if it fails print an error message and exit
+    int addrlen = sizeof(this->address);
+    new_sock = accept(this->server_sock, (struct sockaddr *)&this->address, (socklen_t*)&addrlen);
+    if ( new_sock < 0){
+        perror("Failure in accept() in TCPServer.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //send(new_sock, initMessage.c_str(), strlen(initMessage.c_str()), 0); 
+    sockSend(new_sock, initMessage.c_str());
+    std::cout << "Sent welcome message to client socket: " << new_sock << std::endl; 
+
+    while(true){
+        readEcho();
+        if(strcmp(buf, "quit") == 0){ 
+            break;
+        }
+    }
+    
 }
 
 /**********************************************************************************************
@@ -40,4 +185,8 @@ void TCPServer::listenSvr() {
  **********************************************************************************************/
 
 void TCPServer::shutdown() {
+    if( close(server_sock) ){
+        perror("Failure in close() in TCPServer.");
+        exit(EXIT_FAILURE);
+    }
 }
